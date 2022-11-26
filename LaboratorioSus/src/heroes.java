df88f354b8facd6b6f487b015faaa75118e12cdc
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2022-11-25 23:14:03 -0600

    Descriptions

--- LaboratorioSus/src/heroes.java
+++ LaboratorioSus/src/heroes.java
@@ -4,8 +4,11 @@
 
 /**
  * @author fabi
+ * Manage all the heroes we can use
  *
  */
 public class heroes {
+	
+	
 
 }
--- LaboratorioSus/src/villians.java
+++ LaboratorioSus/src/villians.java
@@ -3,7 +3,8 @@
  */
 
 /**
- * @author fabio
+ * @author fabi
+ * It manage all the villians we can use
  *
  */
 public class villians {
