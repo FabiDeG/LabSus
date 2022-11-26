39904ade597dbd736e4d0044f4301057bc2a542e
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2022-11-25 23:26:30 -0600

    Mosters and heroes var

--- LaboratorioSus/src/heroes.java
+++ LaboratorioSus/src/heroes.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -9,6 +11,12 @@
  */
 public class heroes {
 	
+	private int selection; //Selection of what character we are going to use
+	private int weapon; //Selection of weapon is going to be used	
+	private ArrayList<String> wizardattack; //Here are all the attacks that the wizard can use
+	private ArrayList<String> warriorattack; //Here are all the attacks that the warrior can use
+	private ArrayList<String> arquerattack; //Here are all the attacks that the arguer can use
+	private int life; //This is the life that the heroe have
 	
 
 }
--- LaboratorioSus/src/villians.java
+++ LaboratorioSus/src/villians.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
 
 /**
  * @author fabi
- * It manage all the villians we can use
+ * It manage all the villains we can use
  *
  */
 public class villians {
+	
+	private int selection; //Selection of which character we are going to use
+	private ArrayList<String> mosterone; //Here are all the attacks that the wizard can use
+	private ArrayList<String> monstertwo; //Here are all the attacks that the warrior can use
+	private ArrayList<String> monsterthree; //Here are all the attacks that the arguer can use
+	private ArrayList<String> monsterfour; //Here are all the attacks that the arguer can use
+	private ArrayList<String> monsterfive; //Here are all the attacks that the arguer can use
+	private ArrayList<String> boss; //Here are all the attacks that the arguer can use
+	private int life; //This is the life that the monster have
+	private int limit; //This is the limit that some monsters are going to have
 
+	
 }
